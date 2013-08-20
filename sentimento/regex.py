70a62e841fc878f5b40fad19ecdeae84e8c73156
theorm <theorm@gmail.com>
2013-08-20 16:17:57 +1000

    - fixed regexps a bit

--- sentimento/regex.py
+++ sentimento/regex.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
+import re
 
 POSITIVE_TEXT_FINDER = '|'.join([
-    '[=\:\;]-?[\)D3]', '<3', 'amazing', 'w00t', 'lol', 'rofl'
+    '[=:;]-?[\)D3]', '<3', 'amazing', 'w00t', 'lol', 'rofl'
 ])
 
 NEGATIVE_TEXT_FINDER = '|'.join([
-    '[=\:;]-?[\(\|\\]', 'fail', 'fuck[^ing] (up)?', 'shit', 'sucks', 
+    '[=:;]-?[\(\|\\\]', 'fail', 'fuck', 'shit', 'sucks', 
     'wrong', 'ruin', 'screw'
 ])
+
+if __name__ == '__main__':
+    for t in [':)', ':D', '=)', '<3', 'LOL']:
+        assert re.match(POSITIVE_TEXT_FINDER, t, re.IGNORECASE), '+ {}'.format(t)
+
+
+    for t in [':(', ':(((', 'SHIT', 'Fucked UP', 'screw you']:
+        assert re.match(NEGATIVE_TEXT_FINDER, t, re.IGNORECASE), '- {}'.format(t)
